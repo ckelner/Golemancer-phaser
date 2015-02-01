be0cd88d061c3d49707491d1273a72e8ba9ed519
ckelner <chris.kelner@gmail.com>
2015-02-01 14:04:53 -0500

    add init js

---
+++ src/js/golemancer/init.js
@@ -0,0 +1,25 @@
+/*
+  - Initializes Phaser and Golemancer game objects
+  - Starts the Golemancer game
+*/
+
+// setup custom Golemancer game object
+Golemancer.width = tile_size*cols;
+Golemancer.height = tile_size*rows;
+Golemancer.camera = { x:0, y:0 };
+Golemancer.scene = 1;
+Golemancer.load_progress = 0; // set to value 0 through 100
+
+// create phaser game object
+Golemancer.game = new Phaser.Game(
+  Golemancer.width,
+  Golemancer.height,
+  Phaser.CANVAS,
+  'golemancer'
+);
+
+// game states
+Golemancer.game.state.add('Preload', Golemancer.Preload);
+
+// start the game
+Golemancer.game.state.start('Preload');
