4ee6a6bacc76389e01f0547c70236df27d848f99
ckelner <chris.kelner@gmail.com>
2015-02-01 15:20:21 -0500

    preload bar changes

--- src/js/golemancer/state.preload.js
+++ src/js/golemancer/state.preload.js
@@ -3,10 +3,15 @@ Golemancer.Preload = function(game){
 };
 
 Golemancer.Preload.prototype = {
-  preload: function(){
-    this.preload_bar = this.add.sprite( 1404, 66, 'progress_bar' );
-    this.preload_bar.anchor.setTo( 0.5, 0.5 );
-    this.load.setPreloadSprite( this.preload_bar );
+  preload: function() {
+    this.preload_bar = this.add.sprite(
+      // position center minus half the width/height of bar
+      (Golemancer.width/2)-351,
+      (Golemancer.height/2)-16,
+      'progress_bar'
+    );
+    this.game.load.setPreloadSprite( this.preload_bar );
+    this.game.load.audio( 'title_music', 'assets/music/title.mp3' );
   },
   create: function(){
     //Load menu
