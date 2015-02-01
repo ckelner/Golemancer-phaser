12b9171ed96d1c6945716301ab6d9a5e034c7702
ckelner <chris.kelner@gmail.com>
2015-02-01 16:25:58 -0500

    swap music to title

--- src/js/golemancer/state.menu.js
+++ src/js/golemancer/state.menu.js
@@ -5,22 +5,22 @@ Golemancer.Menu = function( game ) {
 Golemancer.Menu.prototype =  {
   create: function() {
     this.setupMenu();
+    // stop any ongoing music
+    if( Golemancer.game.music ) {
+      Golemancer.game.music.stop();
+    }
+    // start title music
+    Golemancer.game.music = Golemancer.title_music;
+    Golemancer.game.music.volume = 0.5;
+    /* arguements:
+      marker? (leave blank ''), start position (0),
+      volume (full volume), loop (true)
+    */
+    Golemancer.game.music.play( '' , 0, 1, true );
   },
   update: function() {
     if( this.game.input.activePointer.isDown ) {
       //this.game.state.start( 'Workshop' );
-      // stop any ongoing music
-      if( Golemancer.game.music ) {
-        Golemancer.game.music.stop();
-      }
-      // start title music
-      Golemancer.game.music = Golemancer.title_music;
-      Golemancer.game.music.volume = 0.5;
-      /* arguements:
-        marker? (leave blank ''), start position (0),
-        volume (full volume), loop (true)
-      */
-      Golemancer.game.music.play( '' , 0, 1, true );
     }
   },
   setupMenu: function() {
