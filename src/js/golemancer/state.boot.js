b80791a6120e4cdc389e2eabec03d830b6a8c2e2
ckelner <chris.kelner@gmail.com>
2015-02-01 15:19:37 -0500

    spacing updates

--- src/js/golemancer/state.boot.js
+++ src/js/golemancer/state.boot.js
@@ -1,13 +1,13 @@
-Golemancer.Boot = function(game){
+Golemancer.Boot = function( game ) {
   this.game = game;
 };
 
 Golemancer.Boot.prototype = {
-  preload: function(){
+  preload: function() {
     this.load.image( 'progress_bar', 'assets/ui/progress_bar.png' );
     this.game.stage.backgroundColor = '#000'; // set to black
   },
-  create: function(){
+  create: function() {
    this.game.state.start('Preload');
   }
 };
