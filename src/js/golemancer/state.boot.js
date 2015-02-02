a9a2658d1b579ed62db09ef99e613d29891ebb93
ckelner <chris.kelner@gmail.com>
2015-02-01 21:20:04 -0500

    comments for boot.js

--- src/js/golemancer/state.boot.js
+++ src/js/golemancer/state.boot.js
@@ -4,8 +4,10 @@ Golemancer.Boot = function( game ) {
 
 Golemancer.Boot.prototype = {
   preload: function() {
+    // progress bar
     this.load.image( 'progress_bar', 'assets/ui/progress_bar.png' );
-    this.game.stage.backgroundColor = '#000'; // set to black
+    // bg color
+    this.game.stage.backgroundColor = '#000';
   },
   create: function() {
    this.game.state.start('Preload');
